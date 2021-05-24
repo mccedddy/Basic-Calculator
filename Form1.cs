54ea29f1390335970ac199ff66b341baeb7a08c4
Daniel John Cedric Macallan <cedricmacallan@gmail.com>
2021-05-24 20:57:02 +0800

    Fixed negative numbers

--- Form1.cs
+++ Form1.cs
@@ -12,16 +12,18 @@ namespace Basic_Calculator
 {
     public partial class Basic_Calculator : Form
     {
-        string MDAS = "";
-        string num1;
-        string num2;
-        double result;
         public Basic_Calculator()
         {
             InitializeComponent();
         }
+        // Fields
+        string MDAS = "";
+        string num1;
+        string num2;
+        double result;
         private void btn_Click(object sender, EventArgs e)
         {
+            
             Button button = (Button)sender;
             if (button.Text == ".")
             {
@@ -36,34 +38,34 @@ namespace Basic_Calculator
             else if (button.Text == "0")
             {
                 // If display.text is not 0, add 0
-                if (display.Text != "0") 
-                { display.Text = display.Text + "0"; }
+                if (display.Text == "0") 
+                { display.Text = display.Text; }
 
                 // If display.text is 0, don't add 0
                 else
-                { display.Text = display.Text; }
+                { display.Text = display.Text + "0"; }
             }
             else
             { 
-                // If display.text is 0, remove the text and input button.text
+                // If display.text is 0, remove the text
                 if (display.Text == "0") 
                 { display.Text = ""; }
+
+                // Input button.text
                 display.Text = display.Text + button.Text;
             }
         }
         private void btn_MDAS(object sender, MouseEventArgs e)
         {
             Button button = (Button)sender;
-
-            // If there's still no result, set display.text as num1
-            if (result == 0) 
-            { num1 = num1 + display.Text; }
+            // Set display.text as num1
+            num1 = display.Text;
 
             // If there's still no MDAS, set button.text as MDAS
             if (MDAS == "")
             { MDAS = MDAS + button.Text; }
 
-            // If there's already MDAS, clear MDAS and add button.text as MDAS
+            // If there's already MDAS, clear MDAS and set button.text as MDAS
             else
             {
                 MDAS = "";
@@ -76,11 +78,11 @@ namespace Basic_Calculator
         private void btn_Equals(object sender, EventArgs e)
         {
             // Set display.text as num2
-            num2 = num2 + display.Text;
+            num2 = display.Text;
 
             // Perform operations according to MDAS
             switch (MDAS)
-            {
+            { 
                 case "+":
                     result = double.Parse(num1) + double.Parse(num2);
                     break;
@@ -98,27 +100,19 @@ namespace Basic_Calculator
             // Display result
             display.Text = Convert.ToString(result);
 
-            // Set result as num1
-            num1 = Convert.ToString(result);
-
             // Clear num2 and MDAS
             num2 = "";
             MDAS = "";
         }
         private void btn_negative_Click(object sender, EventArgs e)
         {
-            // Set display.text as variable negative
-            double negative = double.Parse(display.Text);
+            // If display.text is positive, add negative sign
+            if (!display.Text.Contains("-")) 
+            { display.Text = display.Text.Insert(0, "-"); }
 
-            // If the number is positive, subtract twice of its value to make it negative
-            if (negative - negative == 0) { negative = negative - negative * 2; }
-
-            // If the number is negative, subtract twice of its value to make it positive
-            else { negative = negative - negative * 2; }
-
-            // Set the number as num1 and show on display
-            num1 = Convert.ToString(negative);
-            display.Text = num1;
+            // If display.text is negative, remove negative sign
+            else
+            { display.Text = display.Text.Remove(0,1); }
         }
         private void btn_C_Click(object sender, EventArgs e)
         {
