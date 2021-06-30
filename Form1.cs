f1312a2c835d3a8128b91c3db95da108e0922142
Daniel John Cedric Macallan <cedricmacallan@gmail.com>
2021-06-30 18:27:06 +0800

    Removed methods

--- Form1.cs
+++ Form1.cs
@@ -55,13 +55,53 @@ namespace Basic_Calculator
             // If the decimal button is pressed
             if (button.Text == ".")
             {
-                decimalpoint();
+                // If display.text does not have a decimal point, add a decimal point
+                if (!display.Text.Contains("."))
+                {
+                    display.Text = display.Text + ".";
+
+                    // If display.text is ".", set display.text as "0."
+                    if (display.Text == ".")
+                    { display.Text = "0."; }
+                }
+
+                // If an operation has been performed, set display.text as "0." and reset operationDone
+                if (operationDone == true)
+                {
+                    display.Text = "0.";
+                    operationDone = false;
+                }
             }
 
             // If the zero button is pressed
             else if (button.Text == "0")
             {
-                zeroinput();
+                // If zeroError is true, reset zero error and display.text
+                if (zeroError == true)
+                {
+                    zeroError = false;
+                    display.Text = "0";
+                    btn_add.Enabled = true;
+                    btn_subtract.Enabled = true;
+                    btn_multiply.Enabled = true;
+                    btn_divide.Enabled = true;
+                    btn_negative.Enabled = true;
+                    btn_point.Enabled = true;
+                }
+
+                // If display.text is not 0
+                if (display.Text != "0")
+                {
+                    // Add 0
+                    display.Text = display.Text + "0";
+                }
+
+                // If an operation has already been performed, set display.text as 0 and reset operationDone
+                if (operationDone == true)
+                {
+                    display.Text = "0";
+                    operationDone = false;
+                }
 
                 // If there is an MDAS, set display.text as num2
                 if (MDAS != "")
@@ -72,7 +112,17 @@ namespace Basic_Calculator
             else
             {
                 // If zeroError is true, reset zeroError and display.text
-                fixZeroError();
+                if (zeroError == true)
+                {
+                    zeroError = false;
+                    display.Text = "0";
+                    btn_add.Enabled = true;
+                    btn_subtract.Enabled = true;
+                    btn_multiply.Enabled = true;
+                    btn_divide.Enabled = true;
+                    btn_negative.Enabled = true;
+                    btn_point.Enabled = true;
+                }
 
                 // If display.text is 0 or an operation has already been performed
                 if (display.Text == "0" | operationDone == true)
@@ -98,7 +148,21 @@ namespace Basic_Calculator
             if (num1 != "" && num2 != "" && MDAS != "")
             {
                 // Solve and show result
-                operation();
+                switch (MDAS)
+                {
+                    case "+":
+                        result = double.Parse(num1) + double.Parse(num2);
+                        break;
+                    case "-":
+                        result = double.Parse(num1) - double.Parse(num2);
+                        break;
+                    case "x":
+                        result = double.Parse(num1) * double.Parse(num2);
+                        break;
+                    case "/":
+                        result = double.Parse(num1) / double.Parse(num2);
+                        break;
+                }
                 display.Text = Convert.ToString(result);
 
                 // Set display.text as num1 and clear num2
@@ -122,144 +186,40 @@ namespace Basic_Calculator
                 MDAS = MDAS + button.Text;
             }
 
-            // Check if there is an error
-            checkZeroError();
-        }
-        private void btn_Equals(object sender, EventArgs e)
-        {
-            // Set display.text as num2
-            num2 = display.Text;
-
-            // Perform operations according to MDAS
-            operation();
-
-            // Show result
-            display.Text = Convert.ToString(result);
-
-            // Check if there is an error
-            checkZeroError();
-
-            // Clear num2 and MDAS
-            num2 = "";
-            MDAS = "";
-
-            // Set operationDone as true
-            operationDone = true;
-        }
-        private void btn_negative_Click(object sender, EventArgs e)
-        {
-            // If display.text is positive, add negative sign
-            if (!display.Text.Contains("-") && display.Text != "0")
-            { display.Text = display.Text.Insert(0, "-"); }
-
-            // If display.text is negative, remove negative sign
-            else
-            {
-                if (display.Text != "0")
-                { display.Text = display.Text.Remove(0, 1); }
-            }
-        }
-        private void btn_C_Click(object sender, EventArgs e)
-        {
-            // Clear all
-            clearall();
-            checkZeroError();
-        }
-        private void btn_CE_Click(object sender, EventArgs e)
-        {
-            // Clear display
-            display.Text = "0";
-
-            // If zeroError is true, clear all
-            if (zeroError == true)
-            {
-                clearall();
-                checkZeroError();
-            }
-        }
-
-        // Methods
-        private void decimalpoint()
-        {
-            // If display.text does not have a decimal point, add a decimal point
-            if (!display.Text.Contains("."))
-            {
-                display.Text = display.Text + ".";
-
-                // If display.text is ".", set display.text as "0."
-                if (display.Text == ".")
-                { display.Text = "0."; }
-            }
-
-            // If an operation has been performed, set display.text as "0." and reset operationDone
-            if (operationDone == true)
-            {
-                display.Text = "0.";
-                operationDone = false;
-            }
-        }
-        private void zeroinput()
-        {
-            // If zeroError is true
-            fixZeroError();
-
-            // If display.text is not 0
-            if (display.Text != "0")
-            {
-                // Add 0
-                display.Text = display.Text + "0";
-            }
-
-            // If an operation has already been performed, set display.text as 0 and reset operationDone
-            if (operationDone == true)
-            {
-                display.Text = "0";
-                operationDone = false;
-            }
-        }
-        private void operation()
-        {
-            // Solve according to MDAS
-            switch (MDAS)
-            {
-                case "+":
-                    result = double.Parse(num1) + double.Parse(num2);
-                    break;
-                case "-":
-                    result = double.Parse(num1) - double.Parse(num2);
-                    break;
-                case "x":
-                    result = double.Parse(num1) * double.Parse(num2);
-                    break;
-                case "/":
-                    result = double.Parse(num1) / double.Parse(num2);
-                    break;
-            }
-        }
-        private void clearall()
-        {
-            // Reset all variables
-            MDAS = "";
-            num1 = "";
-            num2 = "";
-            result = 0;
-            display.Text = "0";
-            zeroError = false;
-            operationDone = false;
-            checkZeroError();
-        }
-        private void checkZeroError()
-        {
             // If display.text is infinite or NaN, show error and check zero error
             if (display.Text == "∞" | display.Text == "-∞")
             {
-                clearall();
+                // Reset all variables
+                MDAS = "";
+                num1 = "";
+                num2 = "";
+                result = 0;
+                display.Text = "0";
+                operationDone = false;
+                btn_add.Enabled = true;
+                btn_subtract.Enabled = true;
+                btn_multiply.Enabled = true;
+                btn_divide.Enabled = true;
+                btn_negative.Enabled = true;
+                btn_point.Enabled = true;
                 zeroError = true;
                 display.Text = "Cannot divide by zero";
             }
             if (display.Text == "NaN")
             {
-                clearall();
+                // Reset all variables
+                MDAS = "";
+                num1 = "";
+                num2 = "";
+                result = 0;
+                display.Text = "0";
+                operationDone = false;
+                btn_add.Enabled = true;
+                btn_subtract.Enabled = true;
+                btn_multiply.Enabled = true;
+                btn_divide.Enabled = true;
+                btn_negative.Enabled = true;
+                btn_point.Enabled = true;
                 zeroError = true;
                 display.Text = "Result is undefined";
             }
@@ -284,13 +244,152 @@ namespace Basic_Calculator
                 btn_point.Enabled = true;
             }
         }
-        private void fixZeroError()
+        private void btn_Equals(object sender, EventArgs e)
         {
+            // Set display.text as num2
+            num2 = display.Text;
+
+            // Perform operations according to MDAS
+            switch (MDAS)
+            {
+                case "+":
+                    result = double.Parse(num1) + double.Parse(num2);
+                    break;
+                case "-":
+                    result = double.Parse(num1) - double.Parse(num2);
+                    break;
+                case "x":
+                    result = double.Parse(num1) * double.Parse(num2);
+                    break;
+                case "/":
+                    result = double.Parse(num1) / double.Parse(num2);
+                    break;
+            }
+
+            // Show result
+            display.Text = Convert.ToString(result);
+
+            // If display.text is infinite or NaN, show error and check zero error
+            if (display.Text == "∞" | display.Text == "-∞")
+            {
+                // Reset all variables
+                MDAS = "";
+                num1 = "";
+                num2 = "";
+                result = 0;
+                display.Text = "0";
+                zeroError = false;
+                operationDone = false;
+                btn_add.Enabled = true;
+                btn_subtract.Enabled = true;
+                btn_multiply.Enabled = true;
+                btn_divide.Enabled = true;
+                btn_negative.Enabled = true;
+                btn_point.Enabled = true;
+
+                zeroError = true;
+                display.Text = "Cannot divide by zero";
+            }
+            if (display.Text == "NaN")
+            {
+                // Reset all variables
+                MDAS = "";
+                num1 = "";
+                num2 = "";
+                result = 0;
+                display.Text = "0";
+                zeroError = false;
+                operationDone = false;
+                btn_add.Enabled = true;
+                btn_subtract.Enabled = true;
+                btn_multiply.Enabled = true;
+                btn_divide.Enabled = true;
+                btn_negative.Enabled = true;
+                btn_point.Enabled = true;
+
+                zeroError = true;
+                display.Text = "Result is undefined";
+            }
+
+            // if zero error is true, disable MDAS, negative and decimal buttons
             if (zeroError == true)
             {
-                zeroError = false;
+                btn_add.Enabled = false;
+                btn_subtract.Enabled = false;
+                btn_multiply.Enabled = false;
+                btn_divide.Enabled = false;
+                btn_negative.Enabled = false;
+                btn_point.Enabled = false;
+            }
+            else
+            {
+                btn_add.Enabled = true;
+                btn_subtract.Enabled = true;
+                btn_multiply.Enabled = true;
+                btn_divide.Enabled = true;
+                btn_negative.Enabled = true;
+                btn_point.Enabled = true;
+            }
+
+            // Clear num2 and MDAS
+            num2 = "";
+            MDAS = "";
+
+            // Set operationDone as true
+            operationDone = true;
+        }
+        private void btn_negative_Click(object sender, EventArgs e)
+        {
+            // If display.text is positive, add negative sign
+            if (!display.Text.Contains("-") && display.Text != "0")
+            { display.Text = display.Text.Insert(0, "-"); }
+
+            // If display.text is negative, remove negative sign
+            else
+            {
+                if (display.Text != "0")
+                { display.Text = display.Text.Remove(0, 1); }
+            }
+        }
+        private void btn_C_Click(object sender, EventArgs e)
+        {
+            // Reset all variables
+            MDAS = "";
+            num1 = "";
+            num2 = "";
+            result = 0;
+            display.Text = "0";
+            zeroError = false;
+            operationDone = false;
+            btn_add.Enabled = true;
+            btn_subtract.Enabled = true;
+            btn_multiply.Enabled = true;
+            btn_divide.Enabled = true;
+            btn_negative.Enabled = true;
+            btn_point.Enabled = true;
+        }
+        private void btn_CE_Click(object sender, EventArgs e)
+        {
+            // Clear display
+            display.Text = "0";
+
+            // If zeroError is true, clear all
+            if (zeroError == true)
+            {
+                // Reset all variables
+                MDAS = "";
+                num1 = "";
+                num2 = "";
+                result = 0;
                 display.Text = "0";
-                checkZeroError();
+                zeroError = false;
+                operationDone = false;
+                btn_add.Enabled = true;
+                btn_subtract.Enabled = true;
+                btn_multiply.Enabled = true;
+                btn_divide.Enabled = true;
+                btn_negative.Enabled = true;
+                btn_point.Enabled = true;
             }
         }
     }
